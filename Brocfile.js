3b1e14aa228706068187e9e7b980be62433d44db
Eric Lifka <eric.lifka@gmail.com>
2014-08-07 22:46:37 -0400

    add lodash

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('vendor/lodash/dist/lodash.js');
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
