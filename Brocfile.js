d2ec6a42b0b1284ced261464b7954850a61b6707
Eric Lifka <eric.lifka@gmail.com>
2014-08-07 23:57:38 -0400

    add bootstrap

--- Brocfile.js
+++ Brocfile.js
@@ -1,22 +1,29 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
 
 var app = new EmberApp();
 
 app.import('vendor/lodash/dist/lodash.js');
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+app.import('vendor/bootstrap/dist/js/bootstrap.js');
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
 
-module.exports = app.toTree();
+var fonts = pickFiles('vendor/bootstrap/dist/fonts', {
+    srcDir: '/',
+    files: ['**/*'],
+    destDir: '/fonts'
+});
+
+var bootstrapCssMap = pickFiles('vendor/bootstrap/dist/css/', {
+    srcDir: '/',
+    files: ['bootstrap.css.map'],
+    destDir: '/assets'
+});
+
+var tree = mergeTrees([app.toTree(), fonts, bootstrapCssMap]);
+
+module.exports = tree;
+
