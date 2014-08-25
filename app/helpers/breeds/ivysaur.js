3bf4bc6f1f123a5ad3c9b4e2c86fb859a3cbb8d9
Eric Lifka <eric.lifka@gmail.com>
2014-08-24 21:35:42 -0400

    add a few pokes

---
+++ app/helpers/breeds/ivysaur.js
@@ -0,0 +1,13 @@
+export default {
+    dex: 2,
+    name: 'ivysaur',
+    types: ['grass', 'poison'],
+    status: {
+        hp: 60,
+        attack: 62,
+        defense: 63,
+        spAttack: 80,
+        spDefense: 80,
+        speed: 60
+    }
+}
