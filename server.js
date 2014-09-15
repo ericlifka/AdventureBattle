136f0188f4035f29e60e0cc0dd3dabcf06004c0f
Eric Lifka <eric.lifka@gmail.com>
2014-09-14 21:40:10 -0400

    add coffee-script compiling to server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var coffeeMiddleware = require('coffee-middleware');
 
 var app = express();
+app.use(coffeeMiddleware({
+    src: __dirname + '/public'
+}));
 app.use(express.static(__dirname + '/public'));
 app.listen(3000);
 
