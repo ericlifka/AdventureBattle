f6d2cecddd94eaf8b618f330adc9f9810bfb4b63
Eric Lifka <eric.lifka@gmail.com>
2014-09-17 23:26:28 -0400

    source maps aren't working and its screwing up chrome debugging, will figure out later

--- coffee.sh
+++ coffee.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # coffee-script is depracating the --join param, which is annoying because the replacements are much heavier weight
-coffee --join app.js --output server/static --map --watch --compile client/
+coffee --join app.js --output server/static --watch --compile client/
