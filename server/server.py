eded3a677df824ebf1709a90c7f62e67d349423a
Eric Lifka <eric.lifka@gmail.com>
2014-09-19 14:53:38 -0400

    set session on login

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import make_response
+from flask import session
 
 app = Flask(__name__)
 
@@ -17,9 +18,13 @@ def login():
     password = request.form['password']
 
     if username in users and users[username] == password:
+        session['username'] = username
         return make_response(('success', '201'))
     else:
         return make_response(('Unauthorized', 401))
+
+# putting secret key in source because TROLLOLOL:
+app.secret_key = 'keep it secret, keep it safe'
 
 if __name__ == "__main__":
     app.run(debug=True)
