b211ed12c004348e7d17fd6d9bd8809be9a05e95
Eric Lifka <eric.lifka@gmail.com>
2014-09-23 20:58:15 -0400

    protect db connections so they always close

--- server/authentication.py
+++ server/authentication.py
@@ -1,26 +1,35 @@
 import sqlite3
 
-def setup_users_table():
+def _db_conn():
     connection = sqlite3.connect('.data.db')
     cursor = connection.cursor()
+    return (connection, cursor)
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)")
-    connection.commit()
+def setup_users_table():
+    (connection, cursor) = _db_conn()
+    try:
+        cursor.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)")
+        connection.commit()
+    finally:
+        connection.close()
 
 def verify_user_password(username, password):
-    connection = sqlite3.connect('.data.db')
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
-    return True if cursor.fetchone() else False
+    (connection, cursor) = _db_conn()
+    try:
+        cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
+        return True if cursor.fetchone() else False
+    finally:
+        connection.close()
 
 def create_user(username, password):
-    connection = sqlite3.connect('.data.db')
-    cursor = connection.cursor()
+    (connection, cursor) = _db_conn()
+    try:
+        cursor.execute('SELECT * FROM users WHERE username=?', (username,))
+        if cursor.fetchone():
+            return False
 
-    cursor.execute('SELECT * FROM users WHERE username=?', (username,))
-    if cursor.fetchone():
-        return False
-
-    cursor.execute('INSERT INTO users VALUES (?, ?)', (username, password))
-    connection.commit()
-    return True
+        cursor.execute('INSERT INTO users VALUES (?, ?)', (username, password))
+        connection.commit()
+        return True
+    finally:
+        connection.close()
