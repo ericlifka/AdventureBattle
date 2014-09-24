9a6c8362a3ed11f02fd74cc8c87da7c689ba5c65
Eric Lifka <eric.lifka@gmail.com>
2014-09-23 21:47:56 -0400

    connection manager version

--- server/authentication.py
+++ server/authentication.py
@@ -1,35 +1,39 @@
 import sqlite3
+from contextlib import contextmanager
 
-def _db_conn():
-    connection = sqlite3.connect('.data.db')
-    cursor = connection.cursor()
-    return (connection, cursor)
+def _connect():
+    return sqlite3.connect('.data.db')
 
-def setup_users_table():
-    (connection, cursor) = _db_conn()
+@contextmanager
+def _db_context(connection):
     try:
-        cursor.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)")
+        yield None
+    except:
+        connection.rollback()
+    else:
         connection.commit()
     finally:
         connection.close()
 
+def setup_users_table():
+    connection = _connect()
+    with _db_context(connection):
+        connection.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)")
+
 def verify_user_password(username, password):
-    (connection, cursor) = _db_conn()
-    try:
+    connection = _connect()
+    with _db_context(connection):
+        cursor = connection.cursor()
         cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
         return True if cursor.fetchone() else False
-    finally:
-        connection.close()
 
 def create_user(username, password):
-    (connection, cursor) = _db_conn()
-    try:
+    connection = _connect()
+    with _db_context(connection):
+        cursor = connection.cursor()
         cursor.execute('SELECT * FROM users WHERE username=?', (username,))
         if cursor.fetchone():
             return False
 
-        cursor.execute('INSERT INTO users VALUES (?, ?)', (username, password))
-        connection.commit()
+        connection.execute('INSERT INTO users VALUES (?, ?)', (username, password))
         return True
-    finally:
-        connection.close()
