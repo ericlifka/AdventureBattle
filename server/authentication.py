90cab79a9e42b0facdd58c9153037824aca70ff2
Eric Lifka <eric.lifka@gmail.com>
2014-09-23 21:52:37 -0400

    fixed the generator assignment

--- server/authentication.py
+++ server/authentication.py
@@ -5,9 +5,10 @@ def _connect():
     return sqlite3.connect('.data.db')
 
 @contextmanager
-def _db_context(connection):
+def _db_context():
+    connection = _connect()
     try:
-        yield None
+        yield connection
     except:
         connection.rollback()
     else:
@@ -16,20 +17,17 @@ def _db_context(connection):
         connection.close()
 
 def setup_users_table():
-    connection = _connect()
-    with _db_context(connection):
+    with _db_context() as connection:
         connection.execute("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)")
 
 def verify_user_password(username, password):
-    connection = _connect()
-    with _db_context(connection):
+    with _db_context() as connection:
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
         return True if cursor.fetchone() else False
 
 def create_user(username, password):
-    connection = _connect()
-    with _db_context(connection):
+    with _db_context() as connection:
         cursor = connection.cursor()
         cursor.execute('SELECT * FROM users WHERE username=?', (username,))
         if cursor.fetchone():
