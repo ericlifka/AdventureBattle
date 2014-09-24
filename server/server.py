d94bffa2059fcd0be66a28b8ec6445f94b7c290a
Eric Lifka <eric.lifka@gmail.com>
2014-09-23 21:05:12 -0400

    switch to using the flask module as a namespace instaed of importing symbols

--- server/server.py
+++ server/server.py
@@ -1,50 +1,46 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import make_response
-from flask import session
+import flask
 import json
 import authentication
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 @app.route("/session", methods=['GET'])
-def checkSession():
-    if 'username' not in session:
-        return make_response(('Unauthorized', 401))
+def check_session():
+    if 'username' not in flask.session:
+        return flask.make_response(('Unauthorized', 401))
     else:
-        return make_response((
+        return flask.make_response((
             json.dumps({
-                'username': session['username']
+                'username': flask.session['username']
             }),
             201
         ))
 
 @app.route("/login", methods=['POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    username = flask.request.form['username']
+    password = flask.request.form['password']
 
     if authentication.verify_user_password(username, password):
-        session['username'] = username
-        return make_response(('success', 200))
+        flask.session['username'] = username
+        return flask.make_response(('success', 200))
     else:
-        return make_response(('unauthorized', 401))
+        return flask.make_response(('unauthorized', 401))
 
 @app.route("/register", methods=['POST'])
 def register():
-    username = request.form['username']
-    password = request.form['password']
+    username = flask.request.form['username']
+    password = flask.request.form['password']
 
     if authentication.create_user(username, password):
-        session['username'] = username
-        return make_response(('success', 201))
+        flask.session['username'] = username
+        return flask.make_response(('success', 201))
     else:
-        return make_response(('unauthorized', 401))
+        return flask.make_response(('unauthorized', 401))
 
 # putting secret key in source because TROLLOLOL:
 app.secret_key = 'keep it secret, keep it safe'
