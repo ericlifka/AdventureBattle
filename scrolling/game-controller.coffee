3e33447018adc35a1ab8ae2c03819563394f57bc
Eric Lifka <eric.lifka@gmail.com>
2014-10-10 20:33:09 -0400

    run animation loop

--- scrolling/game-controller.coffee
+++ scrolling/game-controller.coffee
@@ -2,6 +2,14 @@ window.GameController = class GameController
     constructor: (@viewport) ->
         @stage = new PIXI.Stage 0xf5f5f5
         @renderer = new PIXI.WebGLRenderer 1024, 576
+        @viewport.append @renderer.view
 
     start: ->
-        @viewport.append @renderer.view
+        browserFrameHook = =>
+            @nextAnimationFrame()
+            requestAnimationFrame browserFrameHook
+
+        requestAnimationFrame browserFrameHook
+
+    nextAnimationFrame: ->
+        @renderer.render @stage
--- server/static/scrolling.js
+++ server/static/scrolling.js
@@ -5,9 +5,25 @@
   window.GameController = GameController = (function() {
     function GameController(viewport) {
       this.viewport = viewport;
+      this.stage = new PIXI.Stage(0xf5f5f5);
+      this.renderer = new PIXI.WebGLRenderer(1024, 576);
+      this.viewport.append(this.renderer.view);
     }
 
-    GameController.prototype.start = function() {};
+    GameController.prototype.start = function() {
+      var browserFrameHook;
+      browserFrameHook = (function(_this) {
+        return function() {
+          _this.nextAnimationFrame();
+          return requestAnimationFrame(browserFrameHook);
+        };
+      })(this);
+      return requestAnimationFrame(browserFrameHook);
+    };
+
+    GameController.prototype.nextAnimationFrame = function() {
+      return this.renderer.render(this.stage);
+    };
 
     return GameController;
 
